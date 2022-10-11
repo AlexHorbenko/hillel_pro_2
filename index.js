fa6e46d96e90c4456d6744543602fc5d44725164
AlexHorbenko <agorbenko1501@gmail.com>
2022-10-11 17:26:04 +0300

    task has done

---
+++ index.js
@@ -0,0 +1,9 @@
+const firstNumber = prompt('Enter first number');
+
+const secondNumber = prompt('Enter second number');
+
+const thirdNumber = prompt('Enter third number');
+
+const result = (+firstNumber + +secondNumber + +thirdNumber)/3;
+
+alert(result);
